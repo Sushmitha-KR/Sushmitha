36635bc2669c7aea61e1ec5d4ae43fe6f024bf12
Sushmitha-KR <74805745+Sushmitha-KR@users.noreply.github.com>
2021-10-24 19:02:04 +0530

    Add files via upload

---
+++ NGO-Management-System-master/navbarcut.php
@@ -0,0 +1,10 @@
+ <!--<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Login Here
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="login/adminLogin.php">Admin</a>
+          <a class="dropdown-item" href="login/donorLogin.php">Donor</a>
+          <a class="dropdown-item" href="login/volunteerLogin.php">Volunteer</a>
+        </div>
+      </li>-->
